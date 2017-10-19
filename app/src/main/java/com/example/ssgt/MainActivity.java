bd30cae48df07c76ebc94ae91c9647ec7a3ce8d4
donghyun <kei99134@naver.com>
2017-10-20 02:34:48 +0900

    testing

--- app/src/main/java/com/example/ssgt/MainActivity.java
+++ app/src/main/java/com/example/ssgt/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
+            //hihihihi
 
                 Intent i = new Intent(getApplicationContext(),InterestActivity.class);
                 startActivity(i);
